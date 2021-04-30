13a264363f3d74653913b10e50b2494307dc708a
Belal <bhomaidan@hotmail.com>
2021-04-30 20:32:47 +0200

    add Action Module

--- yuAction.py
+++ yuAction.py
@@ -7,7 +7,7 @@
 '''
 import subprocess
 from RWS4YuMi import RWS4YuMi
-from time import sleep
+from time import sleep, time
 #
 
 #======================
@@ -24,19 +24,66 @@ class actionHandler():
 	    self.plan = plan
 	    self.api = RWS4YuMi()
 
+	def pointCoords(self, point, filepath='points.txt', name=None):
+		"""
+		Function: pointCoords, to map the string point 'p_yy_xx' into robtarget.
+		---
+		Parameters:
+		@param: point, string, the point to be written to RAPID code: ['p_yy_xx', 'p_yy_xxVS', 'p_yy_xxHA', 'p_yy_xxHC'] 
+		@param: filepath, string, thae path to the RAPID code to be modified.
+		---
+		@return: None.
+		"""
+		with open(filepath, 'r') as fp:
+			line = True
+			while(line):
+				line = fp.readline()
+				if(len(line.split())>2):
+					if(line.split()[2]==point):
+						if name is not None:
+							line = line.split()
+							line[2] = name
+							line = " ".join(line)
+						return line
+		print("Point Not Found!")
+		return None
+	
+	def writeLine(self, point, filepath='Rapid/currLeft.mod'):
+		"""
+		Function: writeLine, to write the point robtarget into the RAPID code.
+		---
+		Parameters:
+		@param: point, string, the point to be written to RAPID code: ['p_yy_xx', 'p_yy_xxVS', 'p_yy_xxHA', 'p_yy_xxHC'] 
+		@param: filepath, string, thae path to the RAPID code to be modified.
+		---
+		@return: None.
+		"""
+		name = 'pickpoint'
+		lineIndex = 10
+			
+		if len(point)>7 :
+			name = 'placepoint'
+			lineIndex = 11
+
+		line = "  "+self.pointCoords(point, name=name)
+
+		with open(filepath, 'r') as fp:
+		    codeList = fp.readlines()
+
+		with open(filepath, 'w') as fp:
+			codeList[lineIndex-1] = line+"\n"
+			fp.writelines(codeList)
+
 	def actionInterpreter(self):
-	    """
-	    Function: actionInterpreter, to extract the pick, place, neighbuorhood points, and rotation.
-	    ---
-	    Parameters:
-	    @param: None
-	    ---
-	    @return: pickPos, string of the shape 'p_yy_xx', the position for pick task.
-	             placePos, string of the shape 'p_yy_xx', the position for place task.
-	             rotAngle, integer, [-90, 0, 90] rtoation angle ==> [anticlockwise, no-rotaion, clockwise].
-	             neighborhood, list of strings of the shape 'p_yy_xx', defines the neighbourhood.
-	    """
-	    rotAngle = 0
+		"""
+		Function: actionInterpreter, to interpret the plan into pick & place positions, and neighbours.
+		---
+		Parameters:
+		@param: None
+		---
+		@return: neighborhood, list of point strings to be passed to the GUI.
+		"""
+		rotAngle = 0
 		legoDict = {
 		# y_2x2
 		'y_2x2_11' : 'p_20_00',
@@ -67,11 +114,26 @@ class actionHandler():
 		'b_2x4_53_R' : 'p_23_08',
 		'b_2x4_41_R' : 'p_19_06'
 		}
+		rotDict = {
+		'p_23_02' : ['HA', 'HC'],
+		'p_22_02' : ['HC', 'HA'],
+		'p_21_02' : ['HA', 'HC'],
+		'p_20_02' : ['HC', 'HA'],
+		'p_23_08' : ['HA', 'HC'],
+		'p_22_08' : ['HC', 'HA'],
+		'p_21_08' : ['HA', 'HC'],
+		'p_20_08' : ['HC', 'HA'],
+		'p_19_08' : ['HA', 'HC'],
+		'p_18_08' : ['HC', 'HA'],
+		'p_23_04' : ['HA', 'HC'],
+		'p_22_04' : ['HC', 'HA'],
+		'p_21_04' : ['HA', 'HC'],
+		'p_20_04' : ['HC', 'HA'],
+		'p_19_06' : ['HA', 'HC'],
+		'p_18_06' : ['HC', 'HA'],
+		}
 		# action is a list of action and related params
 		for action in self.plan:
-
-			action[action.index('(')+1:action.index(')')].split()
-
 			if action[0]=='hold_gripper':
 				pickPos = legoDict[action[1]]
 			elif action[0]=='hold_gripper_right':
@@ -80,42 +142,38 @@ class actionHandler():
 				pickPos = legoDict[action[1]+'_L']
 
 			elif action[0] in ['put_2x2_v', 'put_2x2_h']:
-				placePos = action[-3]
+				placePos = action[-3]+"VS"
 				neighborhood = action[-2:]
 			elif action[0] in ['put_upper_4x2', 'put_lower_4x2', 'put_left_2x4', 'put_right_2x4']:
-				placePos = action[-4]
+				placePos = action[-4]+"VS"
 				neighborhood = action[-3:]
 
 			elif action[0] in ['rotate_2x2_loaded_gripper_v2h', 'rotate_2x4_right_loaded_gripper_v2h_clk', 'rotate_2x4_left_loaded_gripper_v2h_clk']:
 				rotAngle = 90
+				placePos[-2:] = rotDict[pickPos][1]
 			elif action[0] in ['rotate_2x4_right_loaded_gripper_v2h_anticlk', 'rotate_2x4_left_loaded_gripper_v2h_anticlk']:
 				rotAngle = -90
+				placePos[-2:] = rotDict[pickPos][0]
 
-		return pickPos, placePos, rotAngle, neighborhood
+		self.writeLine(pickPos)
+		self.writeLine(placePos)
 
-	def pathPlanner(self, srcPoint, dstPoint):
-		"""
-		Function: pathPlanner, to find the path among available paths.
-		---
-		Parameters:
-		@param: srcPoint, string of the shape 'p_yy_xx', start point.
-		@param: dstPoint, string of the shape 'p_yy_xx', end point.
-		---
-		@return: path, list of strings(points) of the shape 'p_yy_xx'
-		"""
-		# paths = [[p1, p2, p3, p4], [p1, p2, p5], ...]
-		paths = []
-		for path in paths:
-			if (path[0] == srcPoint and path[-1] == dstPoint):
-				return path
+		return neighborhood
 
-	def actionMapper(self):
+	# def actionMapper(self):
+	# 	"""
+	# 	Function: actionMapper, to map the plan into action, and pass the neighbourhood to the GUI.
+	# 	---
+	# 	Parameters:
+	# 	@param: None
+	# 	---
+	# 	@return: None
+	# 	"""
+	# 	neighbours = self.actionInterpreter()
+	# 	print(neighbours) # to be passed to the GUI
+	# 	self.action()
+	#     pass
 
-	    pass
-
-	def actionWriter(self):
-
-	    pass
 
 	def action(self):
 		"""
@@ -148,3 +206,15 @@ class actionHandler():
 		if status is None:
 			return False
 		return True
+
+#----------------------------------
+
+# ah = actionHandler(plan)
+# ah.actionMapper()
+
+# tic = time()
+# ah.action()
+
+# if(ah.actionEnded()):
+# 	toc = time()
+# 	print('Done within: {} seconds.'.format(round(toc-tic, 3)))
